8ee522162bd64ce63659b300a39d8a7061407f3a
Akshunn Sharma <77659495+aks117@users.noreply.github.com>
2021-11-28 22:01:37 +0530

    Day-8

---
+++ Day-08/day-8-start.py
@@ -0,0 +1,26 @@
+# Review: 
+# Create a function called greet(). 
+# Write 3 print statements inside the function.
+# Call the greet() function and run your code.
+
+# def greet() :
+
+#   print ("Hello")
+
+#   print ("Namaste")
+
+#   print ("Hallo")
+
+# greet()
+
+# def greeting (name) :
+#   print (f"Hello {name}?")
+#   print (f"How do you do {name}?")
+
+# greeting ("Angeka")
+
+def greetWith(name, location) :
+  print (f"Hello {name}?")
+  print (f"How's the weather today in {location}?")
+
+greetWith(location = "Berlin", name = "Akshunn")
